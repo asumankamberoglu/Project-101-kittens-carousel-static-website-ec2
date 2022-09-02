883b672e6e61f02d5e2d7caf8189febfefc94284
asuman kamberoğlu <asumankamberoglu@gmail.com>
2022-09-02 13:25:25 +0300

    second commit

---
+++ code.sh
@@ -0,0 +1,11 @@
+# #!/bin/bash
+# yum update -y
+# yum install -y httpd
+# systemctl start httpd
+# yum install git -y 
+# git clone https://github.com/asumankamberoglu/Project-101-kittens-carousel-static-website-ec2.git
+# cp var/www/html/Project-101-kittens-carousel-static-website-ec2/static-web/* .
+# cd /var/www/html
+# chmod -R 777 /var/www/html/
+# systemctl restart httpd
+# systemctl enable httpd
